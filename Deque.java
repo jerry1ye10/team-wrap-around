277ffc5f163f992838bfd29c841bfcfc06d8de80
ofrank123 <ofrank@stuy.edu>
2018-04-18 22:10:40 -0400

    Deque interface updated

--- Deque.java
+++ Deque.java
@@ -1,68 +1,103 @@
-public interface Deque<E> {
+public interface Deque {
 	
 	/****************************
 	*     CORE FUNCTIONALITY    *
 	*****************************/
+	//Inserts element passed at the front of the list
+	public void addFirst(String e);
 	
-	public void addFirst(E e);
+	//Inserts element passed at the end of the list
+	public void addLast(String e);
 	
-	public void addLast(E e);
+	//Removes the first element in the list and returns it
+	public String removeFirst();
 	
-	public E removeFirst();
+	//Removes the last element in the list returns it 
+	public String removeLast();
 	
-	public E removeLast();
-	
+	//Returns the size of the list
 	public int size();
 	
-	public E getFirst();
+	//Returns the first element without removing it
+	public String getFirst();
 	
-	public E getLast();
+	//Returns the last element without removing it
+	public String getLast();
 	
 	/****************************
 	*         WRAPPERS          *
 	*****************************/
 	
-	public void add(E e);
+	//Add element to the queue represented by this deque
+	public void add(String e);
 	
-	public E remove();
+	//Remove and return element from the queue represented by this deque
+	public String remove();
 	
-	public E peek();
+	//Returns the first element but does not remove it, returns null if it's empty
+	public String peek();
 	
-	public E peekFirst();
+	//Returns the first element but does not remove it, returns null if it's empty
+	public String peekFirst();
 	
-	public E peekLast();
+	//Returns the last element but does not remove it, returns null if it's empty
+	public String peekLast();
 	
-	public E element();
+	//Returns the first item in the queue represented by this deque without removing it
+	public String element();
 	
-	public boolean offer(E e);
+	//Inserts element into queue represented by deque if permitted by size. return true
+	//on success and false if there's no space.
+	public boolean offer(String e);
 	
-	public boolean offerFirst(E e);
+	//Inserts element at front of dequeue if permitted by size. return true on success
+	//and false if there's no space
+	public boolean offerFirst(String e);
 	
-	public boolean offerLast(E e);
+	//Inserts element at end of dequeue if permitted by size. return true on success
+	//and false if there's no space
+	public boolean offerLast(String e);
 	
+	//Returns and removes first item in queue represented by this deque, and null if the
+	//deque is empty
 	public E poll();
 	
+	//Returns and removes first item in this deque, and null if the
+	//deque is empty
 	public E pollFirst();
 	
+	//Returns and removes first item in this deque, and null if the
+	//deque is empty
 	public E pollList();
 	
+	//Removes and returns an item from the stack represented by this deque
 	public E pop();
 	
-	public void push(E e);
+	//Adds an item onto the stack represented by this deque
+	public void push(String e);
 	
 	/****************************
 	*    EXTRA FUNCTIONALITY    *
 	*****************************/
 	
-	public boolean contains(Object o);
+	//Returns true if the object is in the list
+	public boolean contains(String o);
 	
-	public Iterator<E> iterator();
+	//Return an iterator of the elements in this deque
+	public Iterator<String> iterator();
 	
-	public Iterator<E> descendingIterator();
+	//Return an iterator of the elements in reverse order in this deque
+	public Iterator<String> descendingIterator();
 	
-	public boolean remove(Object o);
+	//Remove the first occurence of Object o in string. Return whether there was an element
+	//that matched the query
+	public boolean remove(String o);
 	
-	public boolean removeFirstOccurence(Object o);
+	//Remove the first occurence of Object o in string. Return whether there was an element
+	//that matched the query
+	public boolean removeFirstOccurence(String o);
 	
-	public boolean removeLastOccurence(Object o);
+	//Remove the last occurince of Object o in string. Return whether there was an element
+	//that matched the query
+	public boolean removeLastOccurence(String o);
 } //end interface
